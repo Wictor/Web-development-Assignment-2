13951cda068902f66f43550f4db3dfa9ff8dacaf
Lucie Saad <lucie.saad@outlook.com>
2018-11-22 15:46:53 +0100

    Lucie changes

---
+++ projekt-html/javaScript/calendarium.js
@@ -0,0 +1,28 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+    if(xhr.status === 200) {
+        myObject = JSON.parse(xhr.responseText);
+
+        var newContent = '';
+        for (var i = 0; i < myObject.events.length; i++) {
+
+            newContent += '<div class="something">';
+            newContent += '<p id="jsdate"><b>' + myObject.events[i].calDate + '</b>';
+            newContent += '<p id="jstext">' + myObject.events[i].text + '<br>';
+            newContent += '<p id="jstime">' + myObject.events[i].time + '<br>';
+            newContent += '</div>';
+
+        }
+
+
+
+
+
+        document.getElementById('upcomingEvents').innerHTML = newContent;
+
+        }
+    };
+
+xhr.open('GET', 'json/calendarium.json', true);
+xhr.send(null);
