9683286fabcb76f27df5bb69869ca8b0299044f5
Joshua Klein Velderman <99joskle@mk.se>
2018-11-28 14:49:12 +0100

    lagt till uppdaterad navbar -josh

--- projekt-html/javaScript/jsnav.js
+++ projekt-html/javaScript/jsnav.js
@@ -7,11 +7,23 @@ window.onload=function() {
         mainNav.classList.toggle('active');
     });
 }
-    function myFunction(id) {
-        var x = document.getElementById(id);
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        }
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
+}
+function openNav() {
+
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginRight = "350px";
+    document.getElementsByClassName("dropdown-icon")[0].transform.rotate = ("-45deg");
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
+
