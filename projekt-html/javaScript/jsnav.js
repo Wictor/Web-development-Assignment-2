3b5fa76b6a24b345ed3c087524fd370cbab6d1fd
Joshua Klein Velderman <99joskle@mk.se>
2018-12-05 10:11:52 +0100

    navbar josh ny

--- projekt-html/javaScript/jsnav.js
+++ projekt-html/javaScript/jsnav.js
@@ -26,4 +26,53 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
 }
+function mysearchFunction() {
+    var y = document.getElementById("my-search-Dropdown");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "block";
+    }
+}
+
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("my-search-Dropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+//
+function mysearchFunction1() {
+    var z = document.getElementById("my-search-Dropdown1");
+    if (z.style.display === "block") {
+        z.style.display = "none";
+    } else {
+        z.style.display = "block";
+    }
+}
+
+
+function filterFunction1() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput1");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("my-search-Dropdown1");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
 
