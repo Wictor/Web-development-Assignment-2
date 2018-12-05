26ff88f00010bcd9fcbb38b6606a0202d81a70da
Joshua Klein Velderman <99joskle@mk.se>
2018-12-05 16:35:11 +0100

    fixat nav, slideshow och to the top knapp

--- projekt-html/javaScript/jsnav.js
+++ projekt-html/javaScript/jsnav.js
@@ -76,3 +76,18 @@ function filterFunction1() {
     }
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        document.getElementById("Topbtn").style.display = "block";
+    } else {
+        document.getElementById("Topbtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
