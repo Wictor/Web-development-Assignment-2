beef9f89f126ce449e781a9b03842520ca7fb84f
Wictor Sundström <wictor.sundstrom@hotmail.com>
2018-12-09 13:46:32 +0100

    position:Fixed Navbar

--- projekt-html/javaScript/jsnav.js
+++ projekt-html/javaScript/jsnav.js
@@ -6,7 +6,8 @@ window.onload=function() {
 
         mainNav.classList.toggle('active');
     });
-}
+};
+
 function myFunction(id) {
     var x = document.getElementById(id);
     if (x.style.display === "block") {
@@ -95,17 +96,6 @@ function filterFunction1() {
         }
     }
 }
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
-        document.getElementById("Topbtn").style.display = "block";
-    } else {
-        document.getElementById("Topbtn").style.display = "none";
-    }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
