0bb8215e2c8c1c937dca62a4b5eaa57f3140f3ff
Wictor Sundstrom <wictor.sundstrom@hotmail.com>
2019-02-27 09:55:47 +0100

    Changed structure of git

--- Assignment 1 - Create web page/JavaScript/resolutionJquary.js
+++
@@ -1,9 +0,0 @@
-$(window).resize(function() {
-    let y = document.getElementById("navBar");
-    if( $(this).width() > 700) {
-        y.style.display = "flex";
-    }
-    else{
-        y.style.display = "none";
-    }
-});
